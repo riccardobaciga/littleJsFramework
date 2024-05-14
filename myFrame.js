3350de62e92c074b1318aab9a842e681e6442b85
riccardobaciga <riccardo@bacigalupo.it>
2024-05-14 23:25:55 +0200

    Create myFrame.js

---
+++ myFrame.js
@@ -0,0 +1,35 @@
+const myAppObj = {};
+
+const myApp = new Proxy(myAppObj, {
+  set(target, property, value) {
+    target[property] = value;
+    if (typeof value !== "object") {
+      const list = document.querySelectorAll('[valueOf="' + property + '"]');
+      if (list.length > 0) {
+        list.forEach((element) => {
+          if (element.nodeName === "INPUT") {
+            element.value = value;
+          } else {
+            element.innerHTML = value;
+          }
+        });
+      }
+      const list2 = document.querySelectorAll('[classOf="' + property + '"]');
+      if (list2.length > 0) {
+        list2.forEach((element) => {
+          element.className = value;
+        });
+      }
+    }
+    return true;
+  },
+  deleteProperty(target, property) {
+    delete target[property];
+    console.log("delete target[property]");
+    return true;
+  },
+});
+
+var _ = function (selector) {
+  return document.querySelector(selector);
+};
