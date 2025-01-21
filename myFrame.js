6ec01e0112f743269016880e2ff850a65763c577
Riccardo <riccardo@bacigalupo.it>
2025-01-21 12:42:18 +0100

    Update myFrame.js

--- myFrame.js
+++ myFrame.js
@@ -3,16 +3,23 @@ const myAppObj = {};
 const myApp = new Proxy(myAppObj, {
   set(target, property, value) {
     target[property] = value;
-    const allowedTypes = ['string', 'number', 'boolean']
+    const allowedTypes = ['string', 'number', 'boolean'];
     if (allowedTypes.includes(typeof value)) {
       const tempValue = myApp.labels ? myApp.labels[property] || value : value;
       const list = document.querySelectorAll('[valueOf="' + property + '"]');
       if (list.length > 0) {
         list.forEach((element) => {
-          if (element.nodeName === "INPUT") {
-            element.value = tempValue;
-          } else {
-            element.innerHTML = tempValue;
+          switch (element.nodeName) {
+            case "INPUT":
+            case "TEXTAREA":
+            case "SELECT":
+              element.value = tempValue;
+              break;
+            case "IMG":
+              element.src = tempValue;
+              break;
+            default:
+              element.innerHTML = tempValue;
           }
         });
       }
@@ -31,46 +38,3 @@ const myApp = new Proxy(myAppObj, {
     return true;
   },
 });
-
-const _ = function (selector) {
-  const found = document.querySelectorAll(selector);
-  var element = found.length == 1 ? found[0] : found;
-  element.hide = function () {
-    if ((element instanceof Element)) {
-      element.classList.remove("visble");
-      element.classList.add("invisible");
-    } else if (element.length > 1) {
-      element.forEach(function (item) {
-        item.classList.remove("visble");
-        item.classList.add("invisible");
-      });
-    }
-  };
-  element.show = function () {
-    if ((element instanceof Element)) {
-      element.classList.add("visble");
-      element.classList.remove("invisible");
-    } else if (element.length > 1) {
-      element.forEach(function (item) {
-        item.classList.add("visble");
-        item.classList.remove("invisible");
-      });
-    }
-  };
-
-  element.onEventDo = function (eventName, functionName, param = null) {
-    if ((element instanceof Element)) {
-      element.addEventListener(eventName, function (param) {
-        (param === null)?functionName():functionName(param);
-      });
-    } else if (element.length > 1) {
-      element.forEach(function (item) {
-        item.addEventListener(eventName, function (param) {
-          (param === null)?functionName():functionName(param);
-        });
-      });
-    }
-  };
-
-  return element;
-};
